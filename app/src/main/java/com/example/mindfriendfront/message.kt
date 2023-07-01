494299519d3061bd085ff76421bb31d212926709
handagyeong <80438961+handagyeong@users.noreply.github.com>
2023-07-01 19:43:15 +0900

    chat ing..

---
+++ app/src/main/java/com/example/mindfriendfront/message.kt
@@ -0,0 +1,49 @@
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mindfriendfront.R
+
+class message : AppCompatActivity() {
+    private lateinit var messageContainer: LinearLayout
+    private lateinit var messageEditText: EditText
+    private lateinit var sendButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+//        setContentView(R.layout.activity_message_item)
+
+        messageContainer = findViewById(R.id.messageContainer)
+        messageEditText = findViewById(R.id.messageEditText)
+        sendButton = findViewById(R.id.sendButton)
+
+        sendButton.setOnClickListener {
+            val message = messageEditText.text.toString().trim()
+            if (message.isNotEmpty()) {
+                addMessage("Me", message)
+                messageEditText.text.clear()
+            }
+        }
+
+        // 가상의 메시지로 초기화
+        addMessage("Other", "hi")
+        addMessage("Me", "hello")
+        addMessage("Other", "nice to meet you")
+    }
+
+    private fun addMessage(sender: String, message: String) {
+        val messageView = LayoutInflater.from(this).inflate(R.layout.activity_message_item, null)
+
+        val senderTextView = messageView.findViewById<TextView>(R.id.senderTextView)
+        val messageTextView = messageView.findViewById<TextView>(R.id.messageTextView)
+
+        senderTextView.text = sender
+        messageTextView.text = message
+
+        messageContainer.addView(messageView)
+    }
+}
