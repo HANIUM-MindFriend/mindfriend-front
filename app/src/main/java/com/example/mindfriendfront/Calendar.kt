98d7a82c8a0b3ee9f1ed1369d9c305f7a0e8eddb
elive7 <parksub020704@gmail.com>
2023-07-01 19:59:45 +0900

    feat 캘린더

--- app/src/main/java/com/example/mindfriendfront/Calendar.kt
+++ app/src/main/java/com/example/mindfriendfront/Calendar.kt
@@ -2,10 +2,27 @@ package com.example.mindfriendfront
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.CalendarView
+
+
 
 class Calendar : AppCompatActivity() {
+
+    private lateinit var calendarView: CalendarView
+    private lateinit var selectedDateTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
+
+        calendarView = findViewById(R.id.calendarView)
+        selectedDateTextView = findViewById(R.id.selectedDateTextView)
+
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            val selectedDate = "${dayOfMonth}/${month + 1}/${year}"
+            selectedDateTextView.text = selectedDate
+        }
+
     }
+
 }
