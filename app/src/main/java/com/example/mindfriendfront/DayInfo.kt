15882b4134e19ab01830eb2ce32ca2f774937ea0
chaehyeon02 <rhee2015@gmail.com>
2023-07-05 01:28:26 +0900

    FEAT title

---
+++ app/src/main/java/com/example/mindfriendfront/DayInfo.kt
@@ -0,0 +1,11 @@
+package com.example.mindfriendfront
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DayInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_day_info)
+    }
+}
