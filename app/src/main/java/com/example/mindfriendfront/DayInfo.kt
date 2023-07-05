d94d5f1304c0c1c93195817d87f806c793de108b
chaehyeon02 <rhee2015@gmail.com>
2023-07-06 01:29:46 +0900

    FEAT dayInfo

--- app/src/main/java/com/example/mindfriendfront/DayInfo.kt
+++ app/src/main/java/com/example/mindfriendfront/DayInfo.kt
@@ -1,11 +1,27 @@
 package com.example.mindfriendfront
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class DayInfo : AppCompatActivity() {
+    private lateinit var btnBack: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_day_info)
+
+//        btnMenu = findViewById(R.id.btnMenu)
+//        btnBack = findViewById(R.id.btnBack)
+
+        // 메뉴 열기 버튼 클릭 시 동작 추가
+//        btnBack.setOnClickListener {
+//            Toast.makeText(applicationContext, "버튼 클릭", Toast.LENGTH_SHORT).show()
+//        }
+
+//        btnBack = findViewById(R.id.btnBack)
+//        btnBack.setOnClickListener {
+//            Toast.makeText(applicationContext, "버튼 클릭", Toast.LENGTH_SHORT).show()
+//        }
     }
 }
