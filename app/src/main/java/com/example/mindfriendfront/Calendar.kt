694ebe4fc47573855c517c33699135b78668840b
elive7 <parksub020704@gmail.com>
2023-07-06 21:35:32 +0900

    feat 미션 글씨랑 체크박스 추가
    클릭한 날짜가 오늘 날짜일 때만 + 버튼 보이고 나머지는 안 보이게

--- app/src/main/java/com/example/mindfriendfront/Calendar.kt
+++ app/src/main/java/com/example/mindfriendfront/Calendar.kt
@@ -9,6 +9,7 @@ import java.util.Calendar
 import android.util.Log
 import android.widget.Button
 import android.view.View
+import android.widget.Toast
 
 class Calendar : AppCompatActivity() {
 
@@ -39,26 +40,33 @@ class Calendar : AppCompatActivity() {
 
         // CalendarView의 날짜 선택 이벤트 리스너 설정
         calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
-            // 날짜가 선택되었을 때 동작 수행
-            val isDateSelected = true // 날짜 선택 여부를 확인하는 조건식을 구현해야 함
-            if (isDateSelected) {
+            val selectedCalendar = Calendar.getInstance()
+            selectedCalendar.set(year, month, dayOfMonth)
+
+            val todayCalendar = Calendar.getInstance()
+
+            if (selectedCalendar.get(Calendar.YEAR) == todayCalendar.get(Calendar.YEAR) &&
+                selectedCalendar.get(Calendar.MONTH) == todayCalendar.get(Calendar.MONTH) &&
+                selectedCalendar.get(Calendar.DAY_OF_MONTH) == todayCalendar.get(Calendar.DAY_OF_MONTH)
+            ) {
+                writeBtn.visibility = View.VISIBLE
+                readBtn.visibility = View.GONE
+            } else {
                 writeBtn.visibility = View.GONE
                 readBtn.visibility = View.VISIBLE
-            } else {
-                readBtn.visibility = View.GONE
-                writeBtn.visibility = View.VISIBLE
             }
         }
 
         // writeBtn 클릭 이벤트 설정
         writeBtn.setOnClickListener {
             // writeBtn 클릭 시 동작 수행
-            Log.d("Calendar", "Button clicked")
+            Toast.makeText(applicationContext, "일기 쓰기", Toast.LENGTH_SHORT).show()
         }
 
         // readBtn 클릭 이벤트 설정
         readBtn.setOnClickListener {
             // readBtn 클릭 시 동작 수행
+            Toast.makeText(applicationContext, "일기 조회", Toast.LENGTH_SHORT).show()
         }
 
 
