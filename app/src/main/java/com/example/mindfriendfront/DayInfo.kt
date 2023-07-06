2ca2f02ab00908b302061070e2f29e29de73155e
chaehyeon02 <rhee2015@gmail.com>
2023-07-06 22:35:55 +0900

    FEAT 이미지 업로드 시도 중

--- app/src/main/java/com/example/mindfriendfront/DayInfo.kt
+++ app/src/main/java/com/example/mindfriendfront/DayInfo.kt
@@ -2,6 +2,7 @@ package com.example.mindfriendfront
 
 import android.R.layout
 import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
@@ -16,38 +17,21 @@ class DayInfo : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_day_info)
 
-//        btnMenu = findViewById(R.id.btnMenu)
         btnBack = findViewById(R.id.btnBack)
         colorView = findViewById(R.id.colorView)
 
-        // 메뉴 열기 버튼 클릭 시 동작 추가
-        btnBack.setOnClickListener { v: View? ->
-            // Toast.makeText(applicationContext, "버튼 클릭", Toast.LENGTH_SHORT).show()
-//            changeViewColor(colorView, "value1")
-        }
-
-//        btnBack = findViewById(R.id.btnBack)
-//        btnBack.setOnClickListener {
+        btnBack.setOnClickListener {
 //            Toast.makeText(applicationContext, "버튼 클릭", Toast.LENGTH_SHORT).show()
-//
-//        }
+            changeViewColor(colorView, "value1")
+        }
     }
-//    fun changeViewColor(view: View, data: String) {
-//        val color: Int = when (data) {
-//            "value1" -> Color.RED
-//            "value2" -> Color.GREEN
-//            "value3" -> Color.BLUE
-//            else -> Color.WHITE
-//        }
-////        view.setBackgroundColor(color)
-//        view.setBackgroundResource(color)
-//    }
-//    fun changeViewColor(view: View, data: String) {
-//        LinearLayout 1page;
-//        1page = (LinearLayout) view.findViewById(R.id.1page);
-//        1page.setBackgroundColor(Color.parseColor("#000000"));
-//
-//        colorView = findViewById(R.id.colorView)
-//
-//    }
+    fun changeViewColor(view: View, data: String) {
+        val color: Int = when (data) {
+            "value1" -> Color.RED
+            "value2" -> Color.GREEN
+            "value3" -> Color.BLUE
+            else -> Color.BLACK
+        }
+        view.setBackgroundColor(color)
+    }
 }
