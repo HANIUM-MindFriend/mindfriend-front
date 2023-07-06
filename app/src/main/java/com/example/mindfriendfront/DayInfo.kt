62e3df9a5810ef404ccdd1dee20705a16511aa01
chaehyeon02 <rhee2015@gmail.com>
2023-07-06 20:44:41 +0900

    FEAT button click

--- app/src/main/java/com/example/mindfriendfront/DayInfo.kt
+++ app/src/main/java/com/example/mindfriendfront/DayInfo.kt
@@ -4,24 +4,42 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.graphics.Color
+import android.widget.ImageButton
 
 class DayInfo : AppCompatActivity() {
-    private lateinit var btnBack: Button
+    private lateinit var btnBack: ImageButton
+    private lateinit var colorView: View
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_day_info)
 
 //        btnMenu = findViewById(R.id.btnMenu)
-//        btnBack = findViewById(R.id.btnBack)
+        btnBack = findViewById(R.id.btnBack)
+        colorView = findViewById(R.id.colorView)
 
         // 메뉴 열기 버튼 클릭 시 동작 추가
-//        btnBack.setOnClickListener {
-//            Toast.makeText(applicationContext, "버튼 클릭", Toast.LENGTH_SHORT).show()
-//        }
+        btnBack.setOnClickListener { v: View? ->
+            // Toast.makeText(applicationContext, "버튼 클릭", Toast.LENGTH_SHORT).show()
+            changeViewColor(colorView, "value1")
+        }
 
 //        btnBack = findViewById(R.id.btnBack)
 //        btnBack.setOnClickListener {
 //            Toast.makeText(applicationContext, "버튼 클릭", Toast.LENGTH_SHORT).show()
+//
 //        }
+    }
+    fun changeViewColor(view: View, data: String) {
+        val color: Int = when (data) {
+            "value1" -> Color.RED
+            "value2" -> Color.GREEN
+            "value3" -> Color.BLUE
+            else -> Color.BLACK
+        }
+        view.setBackgroundColor(color)
+//        android:background="@drawable/dayinfo_color"
+        view.drawableState
     }
 }
