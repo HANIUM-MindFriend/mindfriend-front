1980217543f1f95447b921c055089b903691bfd3
handagyeong <80438961+handagyeong@users.noreply.github.com>
2023-07-08 16:35:22 +0900

    feat: chat 하는중 (아직 메세지안뜸)

--- app/src/main/java/com/example/mindfriendfront/message.kt
+++ app/src/main/java/com/example/mindfriendfront/message.kt
@@ -1,9 +1,11 @@
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mindfriendfront.R
 
@@ -13,6 +15,9 @@ class message : AppCompatActivity() {
     private lateinit var sendButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        Toast.makeText(applicationContext, "홈 클릭", Toast.LENGTH_SHORT).show()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 //        setContentView(R.layout.activity_message_item)
@@ -20,8 +25,10 @@ class message : AppCompatActivity() {
         messageContainer = findViewById(R.id.messageContainer)
         messageEditText = findViewById(R.id.messageEditText)
         sendButton = findViewById(R.id.sendButton)
+        sendButton.setBackgroundColor(Color.parseColor("#7ea9fd"))
 
         sendButton.setOnClickListener {
+
             val message = messageEditText.text.toString().trim()
             if (message.isNotEmpty()) {
                 addMessage("Me", message)
