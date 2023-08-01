056e2cba3f72117e10f6b62dadb9aaa053f28c8f
chaehyeon02 <rhee2015@gmail.com>
2023-08-01 14:32:45 +0900

    feat: 무드트래커

---
+++ app/src/main/java/com/example/mindfriendfront/Analyze.kt
@@ -0,0 +1,45 @@
+package com.example.mindfriendfront
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.GridView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class Analyze : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_analyze)
+
+        // R.layout.activity_analyze 레이아웃의 GridView를 찾아옴
+        val gridView: GridView = findViewById(R.id.gridView)
+
+        // 1부터 30까지의 숫자를 가지는 배열 생성
+        val numbers = Array(30) { i -> (i + 1).toString() }
+
+        // ArrayAdapter를 사용하여 GridView에 데이터 설정
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, numbers)
+        gridView.adapter = adapter
+
+        //각 칸마다 색깔 설정하기
+//        val adapter = object : ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, numbers) {
+//            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+//                val view = super.getView(position, convertView, parent) as TextView
+//                val bgColor = if (position % 2 == 0) Color.RED else Color.BLUE
+//                view.setBackgroundColor(bgColor)
+//                return view
+//            }
+//        }
+
+        gridView.adapter = adapter
+    }
+}
+//위 코드에서 R.drawable.background_image 부분을 설정하고자 하는 배경 이미지의 리소스 아이디로 변경해주시면 됩니다.
+//이렇게 하면 Analyze 클래스의 onCreate 함수에서 GridView의 배경화면이 설정됩니다.
+
+
+
+
+
--- app/src/main/java/com/example/mindfriendfront/DayInfo.kt
+++ app/src/main/java/com/example/mindfriendfront/DayInfo.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
